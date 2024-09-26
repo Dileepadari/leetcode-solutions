5dc368e169fe1630c9720870a2e53c4e841ad3c3
dileepadari <adaridileep@gmail.com>
2024-09-26 23:39:15 +0530

    added three more

--- codeforces.cpp
+++ codeforces.cpp
@@ -2128,3 +2128,37 @@ int main(){
     }
     return 0;
 }
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout << min(a, b) << " ";
+    if(max(a, b) - min(a, b) > 1){
+        cout << (max(a, b) - min(a, b)) / 2 << endl; 
+    }else{
+        cout << 0 << endl;
+    }
+    return 0;
+}
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int a, b, c;
+        cin >> a >> b >> c;
+        if(a+b == c || b+c == a || a+c == b){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
